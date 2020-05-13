a5d45915029c4f18655e24759b7701564fe47fca
Ashutosh730 <Ashu@730>
2020-05-13 18:56:03 +0530

    Get data from parse serve

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -2,48 +2,87 @@ package com.example.instagramclone;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.SaveCallback;
+import com.parse.ParseQuery;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity {
 
     private EditText name,power,punch_speed,kick_speed;
+    private TextView data;
+    private Button all_data;
+    private String alldata;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        all_data=findViewById(R.id.alldata);
+        data=findViewById(R.id.data);
         name=findViewById(R.id.name);
         power=findViewById(R.id.power);
         punch_speed=findViewById(R.id.punch_speed);
         kick_speed=findViewById(R.id.kick_speed);
 
-    }
-
-    public void tapped(View v)
-    {
-        ParseObject kickboxer=new ParseObject("KickBoxer");
-        kickboxer.put("name",name.getText().toString());
-        kickboxer.put("punch_speed",Integer.parseInt(punch_speed.getText().toString()));
-        kickboxer.put("kick_speed",Integer.parseInt(kick_speed.getText().toString()));
-        kickboxer.put("power",Integer.parseInt(power.getText().toString()));
-        kickboxer.saveInBackground(new SaveCallback() {
+        data.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseException e) {
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parse=ParseQuery.getQuery("KickBoxer");
+                parse.getInBackground("vc0gNA4hXo", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
 
-                if(e==null)
-                {
-                    FancyToast.makeText(SignUp.this,"Ur data is saved",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
-                }
-                else
-                    FancyToast.makeText(SignUp.this,"Oops not saved,Try again",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                        if(object!=null&&e==null)
+                        {
+                            data.setText(object.get("name")+"");
+                        }
+
+                    }
+                });
             }
         });
+
+        all_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                alldata="";
+                ParseQuery<ParseObject> all=ParseQuery.getQuery("KickBoxer");
+                all.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+
+                        if(e==null)
+                        {
+                            if(objects.size()>0)
+                            {
+                                for(ParseObject i:objects)
+                                {
+                                    alldata=alldata+i.get("name")+"\n";
+                                }
+                                FancyToast.makeText(SignUp.this,alldata,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            }
+                            else
+                                FancyToast.makeText(SignUp.this,"oops",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                        }
+
+                    }
+                });
+            }
+        });
+
     }
+
 }
